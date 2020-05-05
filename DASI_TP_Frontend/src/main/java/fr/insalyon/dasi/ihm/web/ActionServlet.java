5436dee7a4654cf0df7e175609e1e9271be9117b
Piotr <pitf99@gmail.com>
2020-05-05 11:49:33 +0200

    Change packages

--- DASI_TP_Frontend/src/main/java/fr/insalyon/dasi/ihm/web/ActionServlet.java
+++ DASI_TP_Frontend/src/main/java/fr/insalyon/dasi/ihm/web/ActionServlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dasi_tp_frontend.controleur;
+package fr.insalyon.dasi.ihm.web;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
